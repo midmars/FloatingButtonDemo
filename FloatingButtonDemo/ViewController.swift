c7628a866d6dcf9d51bafea1e34d5eebbf5245b1
Ricky Chen <rickychen@ddim.com.tw>
2024-03-27 15:21:13 +0800

    Initial Commit

---
+++ FloatingButtonDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloatingButtonDemo
+//
+//  Created by 陳耕霈 on 2024/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ FloatingButtonDemoUITests/FloatingButtonDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FloatingButtonDemoUITestsLaunchTests.swift
+//  FloatingButtonDemoUITests
+//
+//  Created by 陳耕霈 on 2024/3/27.
+//
+
+import XCTest
+
+final class FloatingButtonDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
